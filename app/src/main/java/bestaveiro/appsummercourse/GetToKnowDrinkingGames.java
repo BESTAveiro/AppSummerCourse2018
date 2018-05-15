90c162ad9e9d8bbb448d1ce1290fad0de0704e48
Diego Caldeira Hernandez <dc.hernandez@ua.pt>
2018-05-16 00:34:34 +0100

    created: Tasks files

---
+++ app/src/main/java/bestaveiro/appsummercourse/GetToKnowDrinkingGames.java
@@ -0,0 +1,13 @@
+package bestaveiro.appsummercourse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetToKnowDrinkingGames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_to_know_drinking_games);
+    }
+}
