c832cdcb039a9392540c2d0784bcf94ae00df1ac
can1no <alexandrevala@ua.pt>
2018-05-16 19:17:08 +0100

    comments

--- app/src/main/java/bestaveiro/appsummercourse/OrganizersHandbook.java
+++ app/src/main/java/bestaveiro/appsummercourse/OrganizersHandbook.java
@@ -9,5 +9,6 @@ public class OrganizersHandbook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizers_handbook);
+//        vala
     }
 }
