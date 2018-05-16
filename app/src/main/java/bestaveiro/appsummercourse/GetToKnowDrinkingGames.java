fe077c5478321164a21f3ce59acf7a49f9e5ad88
marianajs <mariana.j.santos@ua.pt>
2018-05-16 19:02:36 +0100

    comment

--- app/src/main/java/bestaveiro/appsummercourse/GetToKnowDrinkingGames.java
+++ app/src/main/java/bestaveiro/appsummercourse/GetToKnowDrinkingGames.java
@@ -9,5 +9,6 @@ public class GetToKnowDrinkingGames extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_to_know_drinking_games);
+//        mariana
     }
 }
