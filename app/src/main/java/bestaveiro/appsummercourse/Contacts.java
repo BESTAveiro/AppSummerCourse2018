fce17ff31a09da2d726c6a3ccae134b4952d848a
Diego Caldeira Hernandez <dc.hernandez@ua.pt>
2018-05-30 18:49:38 +0100

    dummy commit

--- app/src/main/java/bestaveiro/appsummercourse/Contacts.java
+++ app/src/main/java/bestaveiro/appsummercourse/Contacts.java
@@ -9,5 +9,8 @@ public class Contacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+//        Diego Caldeira
+        //bwdqbqwdbwqhbi
     }
 }
